aea6b83eb488c82612fe9992e848c3377d5dc231
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-04 11:19:54 -0300

    Create math operations.java

---
+++ math operations.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numero1 = 9; // número inteiro
+        int numero2 = 2;
+
+        System.out.println(numero1+numero2); //adição
+        System.out.println(numero1-numero2); //subtração
+        System.out.println(numero1*numero2); //multiplicação
+        System.out.println(numero1/numero2); //divisão
+        System.out.println(numero1%numero2); //resto da divisão
+/*
+        double numero1 = 9; //número quebrado (com .)
+        double numero2 = 2;
+
+        System.out.println(numero1+numero2); //adição
+        System.out.println(numero1-numero2); //subtração
+        System.out.println(numero1*numero2); //multiplicação
+        System.out.println(numero1/numero2); //divisão
+*/
+    }
+}
