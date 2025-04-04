a8953e600fbe635406678c1a51ec76ab098bfd6d
AkiraSunsets <ketlynlorraynendearaujo@gmail.com>
2025-04-04 10:11:28 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+    //Tipos de variaveis inteiras
+        byte variavelByte = 127; //Padrão Camel Case = first word starts with lowercase letter and seccond word with uppercase letter
+        System.out.println(variavelByte);
+
+        System.out.printf("Tamanho do byte: %d e também %d",variavelByte,10);
+        // printf é o print formatado, onde as váriaveis são trocadas por máscaras de substituição
+        /*
+        * Tipos das mascaras
+        * %d - inteiros - byte, short, int, long
+        * %f - reais - float, double
+        * %c - caractere - char
+        * %s - string
+         */
+        short variavelShort = 32444;
+        System.out.printf("Tamanho do short: %d\n", variavelShort);
+
+        int variavelInt = 2_147_483_647;
+        System.out.printf("Tamanho do int: %d\n", variavelInt);
+
+        long variavelLong = 9_223_372_036_854_775_807L; //Obrigatório colocar a letra L
+        System.out.printf("Tamanho do long: %d\n", variavelLong);
+    }
+}
