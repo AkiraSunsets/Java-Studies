34b98f1a375a8dd0d01991ab9e4b637b5b6775ba
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-10 08:45:38 -0300

    Update scanner.java

--- scanner.java
+++ scanner.java
@@ -2,12 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
         int numero; // inserindo um número inteiro
+        
         Scanner input = new Scanner(System.in); // função input (pedir ao usuário que digite um número
         System.out.println("Digite um número: ");
         numero = input.nextInt();  //receber números inteiros
         System.out.print("O número que você digitou foi: " + numero); // concatenção
         input.close(); // finaliza o scanner, necessário demais!!
+/////////////////////////////////////////////////////////////////////////////////////////////////
+        String palavra; // para escrever letras
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite uma palavra: "); 
+        palavra = entrada.nextLine(); // receber strings
+        System.out.print("A palavra que você digitou foi: " + palavra);
+//////////////////////////////////////////////////////////////////////////////////////////////////
+        String nome, sobrenome; //declarando duas variáveis
 
+        Scanner scanner = new Scanner(System.in); // só declara uma vez
+        System.out.print("Digite seu nome: ");
+        nome = scanner.nextLine(); // lê tudo, incluindo espaços
+        System.out.print("Digite seu sobrenome: ");
+        sobrenome = scanner.nextLine();
+        System.out.printf("O meu nome é %s e meu sobrenome é %s", nome,sobrenome); // %s é máscara para string
+        scanner.close();
     }
 }
