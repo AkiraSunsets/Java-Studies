7e8ced55748a1f5bbe6d4a0f2340f401ee4bbee9
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-17 09:46:56 -0300

    Create Desafio 1.java

---
+++ First Math Exercises/Desafio 1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class exercicio3 {
+    public static void main(String[] args) {
+    double num1 = -3;
+    double num2 = -1;
+
+    double eq = 3*Math.pow(num1/4,-2) + 6 * (Math.pow(3,num2)/4)-4;
+    double eq2 = 7*Math.pow(num1/4,num2)+2;
+    double fim = Math.pow((eq/eq2),num2)+4;
+        System.out.println(fim);
+
+    }
+}
+
+
+
