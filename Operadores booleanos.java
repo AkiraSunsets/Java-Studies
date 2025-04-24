a615f95288f813876d276c2db11b3b09c1930304
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-24 09:27:23 -0300

    Create Operadores booleanos.java

---
+++ Operadores booleanos.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        //Operadores Relacionais - Interprete como perguntas
+
+
+        // == Igualdade
+        System.out.println(10==5+5);
+
+        // != Diferente de
+        System.out.println('A'!='a');
+
+        // > Maior que
+        System.out.println(20>15);
+
+        // < Menor que
+        System.out.println(10<10);
+
+        // >= Maior ou igual
+        System.out.println(15>=15);
+
+        // <= Menor ou igual
+        System.out.println(10<=10);
+        }
+}
