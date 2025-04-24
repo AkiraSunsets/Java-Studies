2e88deb723343496a7753a0c1acb51b192958d1c
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-24 09:27:54 -0300

    Update equals.java

--- equals.java
+++ equals.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String minhastring = new String("Like a Bosch");
+        String minhastring = new String("Like");
         Scanner scanner = new Scanner(System.in);
         minhastring = scanner.nextLine();
-        System.out.println(minhastring.equals("Like a Bosch")); //Comparação de objetos eu sempre utilizo o equals
+        System.out.println(minhastring.equals("Like")); //Comparação de objetos eu sempre utilizo o equals
     }
 }
