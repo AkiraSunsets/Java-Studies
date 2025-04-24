fd6de19a14d96517f83030b7d3c29b117e621b6a
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-24 11:02:33 -0300

    Create condicionais.java

---
+++ Condicionais/condicionais.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        double media = 7.1;
+        int faltas = 20;
+        boolean postura = true;
+
+        /*
+        Para ser aprovado, ele precisa que as 3 sejam verdadeiras.
+        média>=5
+        faltas < 25
+        postura = true
+
+        * se falhar na média = recuperação
+
+        * se falhar nas faltas = sem férias
+
+        * se falhar na postura = chamar para conversar
+
+
+        // Se ele falhar em dois ou mais critérios, será reprovado diretamente
+         */
+        }
+}
+
+/*
+A B	C
+0	0	0
+0	0	1
+0	1	0
+0	1	1
+1	0	0
+1	0	1
+1	1	0
+1	1	1
+*/
