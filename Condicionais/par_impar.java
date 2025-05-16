cc6b6234455ba8d1203122661403401e10446bab
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-05-16 10:20:05 -0300

    Create par_impar.java

---
+++ Condicionais/par_impar.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+    int num = 3;
+
+    String par_impar;
+
+    if(num % 2 == 0){
+        par_impar = "par";
+    }
+    else{
+        par_impar = "impar";
+    }
+        System.out.println(par_impar);
+    }
+}
+
