7c9480cfb764e1d9be9bfd7cdd630c177bb2fd5c
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-15 11:20:47 -0300

    Create Next_Int.java

---
+++ 15.08.2025 - Revisão/Next_Int.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int numero = scanner.nextInt(); //espera receber um número inteiro
+        System.out.println(numero);
+    }
+}
