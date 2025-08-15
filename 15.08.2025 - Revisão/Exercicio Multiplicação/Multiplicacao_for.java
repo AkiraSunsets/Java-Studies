b132f3a1cc275945ff3b3e88128c318442164a8f
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-15 10:43:15 -0300

    Update Multiplicacao_for.java

--- 15.08.2025 - Revisão/Exercicio Multiplicação/Multiplicacao_for.java
+++ 15.08.2025 - Revisão/Exercicio Multiplicação/Multiplicacao_for.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args){
 
-        for (int num1 = 1; num1 <= 10; num1++){
+        for (int num1 = 1; num1 <= 10; num1++) {
 
             System.out.println("Tabuada do " + num1 + ":"); //vai printar 1 banana, vai printar 2 vezes maça e assim até acabar
 
-            for (int j = 1; j <= 10; j++){
+            for (int j = 1; j <= 10; j++) {
                 int Multiplicacao = num1 * j;
                 System.out.printf("\t%d" + "x" + "%d" + "=" + "%d\n", num1, j, Multiplicacao);
             }
+            System.out.println();
         }
     }
 }
