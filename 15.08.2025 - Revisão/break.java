b3daba0a7cbf4b1f0eb74488ca35f4e849cf88d1
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-15 08:37:59 -0300

    Create break.java

---
+++ 15.08.2025 - Revisão/break.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+
+        for (int i = 0; i <=10; i++) {
+
+            if(i==5){
+                break; //quebra o laço de repetição
+            }
+            System.out.println(i); // imprime uma sequencia de 1 a 4 e quebra antes do 5, encerrando o código
+        }
+    }
+}
