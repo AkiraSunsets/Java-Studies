c388fdcaf274ce951f4cef166d815f2caf79ee2d
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-15 08:27:33 -0300

    Create For.java

---
+++ 15.08.2025 - Revisão/For.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+        for (int i = 0; i < 11; i++) { //cria um loop de iteração ++ é incremento
+            //start     //stop  //step
+            
+            System.out.println(i); //Vai imprimir números de 1 a 10
+        }
+    }
+}
