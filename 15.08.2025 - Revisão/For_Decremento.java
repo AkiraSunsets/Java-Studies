5ee1d4eed2876908a1d2f1699040663f0b1602a2
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-15 08:29:44 -0300

    Create For_Decremento.java

---
+++ 15.08.2025 - Revisão/For_Decremento.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+        for (int i = 10; i > 0; i--) { //cria um loop de iteração // -- é um decremento (diminui de 1 em 1)
+            //start     //stop  //step
+
+            System.out.println(i); //Vai imprimir números de 10 a 11
+        }
+    }
+}
