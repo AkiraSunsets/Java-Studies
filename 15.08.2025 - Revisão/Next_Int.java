c3916efe9287de3d56548dc5c25d4a17fc0aea02
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-15 11:24:34 -0300

    Update and rename Next_Int.java to Try_Catch.java

--- 15.08.2025 - Revisão/Next_Int.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int numero = scanner.nextInt(); //espera receber um número inteiro
-        System.out.println(numero);
-    }
-}
---
+++ 15.08.2025 - Revisão/Try_Catch.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try { //try prevenir erros
+            int numero = scanner.nextInt(); //espera receber um número inteiro
+            System.out.println(numero);
+        } catch (Exception error) { //quando o usuário digitar algo que não for número, vai aparecer essa mensagem na tela
+            System.out.println("Digite apenas números");
+        }
+    }
+}
