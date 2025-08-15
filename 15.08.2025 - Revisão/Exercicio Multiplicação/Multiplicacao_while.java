06980b2eacf9b59bd0675c66423dfb0d3cede9f2
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-15 11:04:25 -0300

    Update Multiplicacao_while.java

--- 15.08.2025 - Revisão/Exercicio Multiplicação/Multiplicacao_while.java
+++ 15.08.2025 - Revisão/Exercicio Multiplicação/Multiplicacao_while.java
@@ -6,10 +6,11 @@ public class Main {
         while (contador<= 10){
             int i = 1;
             while (i <= 10) {
-                System.out.println(contador * i);
+                System.out.printf("%d" + "x" + "%d" + "=" + "%d\n", contador, i, contador * i);
                 i++;
             }
             contador++;
+            System.out.println();
         }
     }
 }
