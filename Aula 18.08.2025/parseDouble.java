16cbd81e81c5e99602f71fec382ac1202bae87e0
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-18 10:17:29 -0300

    Create parseDouble.java

---
+++ Aula 18.08.2025/parseDouble.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String numeroEmTexto = scanner.nextLine();
+        //Wrapper:
+        /*
+        Tipos primitivos são básicos, salvam somente valores
+        Wrappers: são complexos, tem valores e funções
+        Exemplos de wrappers: Integer, Double, Boolean, Character
+        Obs: tipos primitivos são sempre com letras minúsculas. Wrappers iniciam com letra maiúscula
+
+        ParseInt -- Converte strings em números inteiros
+         */
+        int numeroEmInt = Integer.parseInt(numeroEmTexto); //converter número em texto
+        Double numeroEmDouble = Double.parseDouble(numeroEmTexto);
+
+        System.out.println(numeroEmDouble+18);
+
+    }
+}
