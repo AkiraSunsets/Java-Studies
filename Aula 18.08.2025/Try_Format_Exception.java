4ff575aaf5140bc3471522802b41a0ff022a78a9
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-18 10:40:58 -0300

    Create Try_Format_Exception.java

---
+++ Aula 18.08.2025/Try_Format_Exception.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        String numeroEmTexto = scanner.nextLine(); //lê textos e salva com string
+
+        try{ //cria uma solução para o erro que possa dar aqui, se o usuário digitar letras
+            int NumeroEmInt = Integer.parseInt(numeroEmTexto); //solicita que somente números sejam inseridos
+            System.out.println("O número que você digitou foi "+NumeroEmInt);
+        }catch (NumberFormatException error){
+            System.out.println("Formato incorreto, digite apenas números"); //ao colocar letras, aparecerá esta mensagem e o processo encerra
+        }
+    }
+}
