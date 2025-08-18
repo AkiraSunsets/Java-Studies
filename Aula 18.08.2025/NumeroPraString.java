f3018b7a7485be9a110f46cb2d0559a8875eb113
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-18 10:24:43 -0300

    Create NumeroPraString.java

---
+++ Aula 18.08.2025/NumeroPraString.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String numeroEmTexto = scanner.nextLine();
+        //Wrapper:
+        /*
+        Tipos primitivos são básicos, salvam somente valores
+        Wrappers: são complexos, tem valores e funções
+        Exemplos de wrappers: Integer, Double, Boolean, Character
+        Obs: tipos primitivos são sempre com letras minúsculas. Wrappers iniciam com letra maiúscula
+
+        ParseInt -- Converte strings em números inteiros
+         */
+        int numeroEmInt = Integer.parseInt(numeroEmTexto); //converter número em texto
+
+        //ParseDouble - converte String em números do tipo double
+        
+        Double numeroEmDouble = Double.parseDouble(numeroEmTexto);
+
+        int numero = 18;
+        String numeroEmString = numero+"";
+
+        System.out.println(numeroEmString+18);
+    }
+}
