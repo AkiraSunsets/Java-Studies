aed98d9ac8b0c11801e1d69bc7de3da2604d1c8b
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-09-12 08:17:44 -0300

    Create add_remove.java

---
+++ Listas/add_remove.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("Kety"); //adiciona novo item a lista
+        System.out.println(lista);
+        lista.remove("Kety"); //remove o item "Kety" da listas=
+        System.out.println(lista);
+    }
+}
