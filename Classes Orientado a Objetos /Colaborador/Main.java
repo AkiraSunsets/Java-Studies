bdadd8aeee5ed87fb357ceddaad6dd1857181f57
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-10-03 10:32:51 -0300

    Update Main.java

--- Classes Orientado a Objetos /Colaborador/Main.java
+++ Classes Orientado a Objetos /Colaborador/Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Colaborador colaborador = new Colaborador(); //Instanciação
-        colaborador.nome = "Marcos";
-        colaborador.cargo = "Técnica de qualidade";
-        colaborador.edv = 92897645;
-        System.out.println("Colaborador: " + colaborador.nome);
+
+          //Colaborador com 1 parâmetro
+          Colaborador colaborador = new Colaborador("Ana Beatriz"); //Instantiation
+          System.out.println("Nome do colaborador: " + colaborador.nome);
+
+          System.out.println("=============================================");
+          //Colaborador com 2 parâmetros
+          Colaborador colaborador2 = new Colaborador("Ana Beatriz", 9092647); //Instantiation
+          System.out.println("Nome: " + colaborador2.nome);
+          System.out.println("EDV: "  + colaborador2.edv);
+
+          System.out.println("=============================================");
+          //Colaborador com 3 parâmetros
+          Colaborador colaborador3 = new Colaborador("Maria", 9290421, "Aprendiz");
+          System.out.println("Nome do colaborador: " + colaborador3.nome);
+          System.out.println("Edv: " + colaborador3.edv);
+          System.out.println("Cargo: " + colaborador3.cargo);
     }
 }
