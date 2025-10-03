3784788012af43805d98e70eb55fa67d7e7f0ced
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-10-03 09:41:52 -0300

    Create Main.java

---
+++ Classes Orientado a Objetos /Colaborador/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Colaborador colaborador = new Colaborador(); //Instanciação
+        colaborador.nome = "Marcos";
+        colaborador.cargo = "Técnica de qualidade";
+        colaborador.edv = 92897645;
+        System.out.println("Colaborador: " + colaborador.nome);
+    }
+}
