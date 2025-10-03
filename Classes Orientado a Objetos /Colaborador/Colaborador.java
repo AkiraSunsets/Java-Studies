eaa6c50bd4adde34a3c9067a41436b348373b8e2
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-10-03 09:58:30 -0300

    Update Colaborador.java

--- Classes Orientado a Objetos /Colaborador/Colaborador.java
+++ Classes Orientado a Objetos /Colaborador/Colaborador.java
@@ -1,5 +1,19 @@
 public class Colaborador {
     int edv;
     String cargo;
-    String nome;
+    String nome; //atributo da classe
+
+    //Construtor padrão existe em toda classe
+
+
+    //Construtor padrão ou sem parâmetros
+    Colaborador() { //função desempenha uma ação
+        System.out.println("O construtor sem parâmetros foi executado");
+    }
+
+    //Construtor com parâmetros
+    Colaborador(String nome){ //nome é função
+        this.nome = nome; //this. se refere ao atributo da classe 
+        System.out.println("Construtor com um parâmetro foi chamado");
+    }
 }
