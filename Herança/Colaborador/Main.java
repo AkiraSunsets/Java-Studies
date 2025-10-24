a6703efa6c8bf9f22c4eb7de35ff771fa5c9deb1
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-10-24 09:55:20 -0300

    Create Main.java

---
+++ Herança/Colaborador/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Colaborador afonso = new Colaborador(92908864, "Afonso", "Técnico de qualidade");
+        System.out.println(afonso.nome);
+    }
+}
