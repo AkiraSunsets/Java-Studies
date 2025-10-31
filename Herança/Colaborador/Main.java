a3a182d3ae8982a4352d4593839e278efad09736
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-10-31 10:37:13 -0300

    Update Main.java

--- Herança/Colaborador/Main.java
+++ Herança/Colaborador/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Usuario usuario = new Usuario("Isabella", "12345678", "isabella@gmail.com");
         usuario.autenticar();
+        UsuarioAdmin usuarioAdmin = new UsuarioAdmin("Murilo", "12345678", "Murilo@gmail.com");
+        usuarioAdmin.autenticar();
     }
 }
