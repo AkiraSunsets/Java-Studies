e4837ddf5104351c33cea135b39a2937d2840551
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-10-31 10:07:37 -0300

    Update Horista.java

--- Herança/Colaborador/Horista.java
+++ Herança/Colaborador/Horista.java
@@ -2,7 +2,24 @@ public class Horista extends Colaborador {
     double horasTrabalhadas;
     double salarioPorHora;
 
-    public Horista (int edv, String nome, String cargo, double HorasTrabalhadas, double salarioPorHora) {
+    public Horista (int edv, String nome, String cargo, double horasTrabalhadas, double salarioPorHora) {
         super(edv, nome, cargo);
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.salarioPorHora = salarioPorHora;
+    }
+    double receberSalario() {
+        return this.salarioPorHora * this.horasTrabalhadas;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Horista{" +
+                "horasTrabalhadas=" + horasTrabalhadas +
+                ", salarioPorHora=" + salarioPorHora +
+                ", edv=" + edv +
+                ", nome='" + nome + '\'' +
+                ", cargo='" + cargo + '\'' +
+                '}';
     }
 }
