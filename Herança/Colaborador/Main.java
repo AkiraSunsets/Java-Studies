348aa0c7c0c7a5863f2b658479d2a3fb2641a66c
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-10-31 10:35:49 -0300

    Update Main.java

--- Herança/Colaborador/Main.java
+++ Herança/Colaborador/Main.java
@@ -2,8 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        Horista Daniela = new Horista(92902233, "Daniela", "Auxiliar Administrativo", 240, 8.50);
-        System.out.printf("O salário de Daniela é: %.2fR$ ", Daniela.receberSalario());
+        Usuario usuario = new Usuario("Isabella", "12345678", "isabella@gmail.com");
+        usuario.autenticar();
     }
 }
