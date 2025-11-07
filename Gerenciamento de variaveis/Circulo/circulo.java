1b70b5646307c4d3cb557fe7677af5b6cbc7b1e4
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-11-07 08:22:45 -0300

    Update circulo.java

--- Gerenciamento de variaveis/Circulo/circulo.java
+++ Gerenciamento de variaveis/Circulo/circulo.java
@@ -1,15 +1,23 @@
 public class Circulo {
+
+    /*
+    * Cor e raio são variaveis dinâmicas, elas mudam
+    * de acordo com o objeto instanciado
+    */
     String cor;
     double raio;
-    double pi = 3.14;
-
+    static double pi = 3.14;
+    /*static é usado para definir um valor fixo, ela pertence a uma classe
+      a cada vez que uma instância se referir ao pi, ele sempre usará o mesmo espaço de memória
+      que foi alocado
+    */
+    
     public Circulo (String cor, double raio) {
         this.cor = cor;
         this.raio = raio;
     }
-    
+
     double calcularArea () {
         return pi*raio*raio;
     }
 }
-    
