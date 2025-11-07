b8679ddd0e75fc065bb1530a8049dd0910636a32
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-11-07 08:58:33 -0300

    Update Main.java

--- Gerenciamento de variaveis/Investimento/Main.java
+++ Gerenciamento de variaveis/Investimento/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Investimento investimentodeAna = new Investimento("Ana", 1000);
         System.out.printf("O investimento de %s está com o saldo de R$%.2f\n", investimentodeAna.nomeDoInvestidor, investimentodeAna.saldoAtual);
+        investimentodeAna.rentabilizar();
+        System.out.printf("O investimento de %s está com o saldo de R$%.2f\n", investimentodeAna.nomeDoInvestidor, investimentodeAna.saldoAtual);
     }
 }
