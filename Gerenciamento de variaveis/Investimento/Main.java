36d2f5ce668fd3e41dbcc6610ade3e6f36437f15
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-11-07 08:56:50 -0300

    Create Main.java

---
+++ Gerenciamento de variaveis/Investimento/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Investimento investimentodeAna = new Investimento("Ana", 1000);
+        System.out.printf("O investimento de %s está com o saldo de R$%.2f\n", investimentodeAna.nomeDoInvestidor, investimentodeAna.saldoAtual);
+    }
+}
