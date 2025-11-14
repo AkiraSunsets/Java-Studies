2f2dbe9ef7b2cec720e8645853f44800a00356bd
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-11-14 11:19:10 -0300

    Update Produto.java

--- Encapsulamento/Produto.java
+++ Encapsulamento/Produto.java
@@ -12,12 +12,24 @@ public class Produto {
     public String getNome(){
         return this.nome;
     }
-    
+
     public double getPreco(){
         return this.preco;
     }
-    
+
     public int getQuantidade(){
         return this.quantidade;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
+
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;
     }
 }
